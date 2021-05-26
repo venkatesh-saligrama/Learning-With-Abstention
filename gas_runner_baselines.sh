5b4f6bdea7b63adf7aa44354fb9f7239997e1d58
Anil Kag <anilkagak2@gmail.com>
2021-05-26 12:03:27 -0400

    Gas weak baseline achieves nearly 71% accuracy (uses only 64 features aka 8 sensors).

--- gas_runner_baselines.sh
+++ gas_runner_baselines.sh
@@ -1,7 +1,7 @@
 
 
-#python gas_train_baseline_model.py -wk  -wd 0.0001  -lr 0.0001 -b 32 -e 100 
+python gas_train_baseline_model.py -wk  -wd 0.001  -lr 0.001 -b 128 -e 100 
 
 
 #python gas_train_baseline_model.py  -wd 0.0001  -lr 0.001 -b 32 -e 100
-python gas_train_baseline_model.py  -wd 0.001  -lr 0.001 -b 128 -e 100
+#python gas_train_baseline_model.py  -wd 0.001  -lr 0.001 -b 128 -e 100
