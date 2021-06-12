a54662f0bb5ddb240e984366075ed6aec10324ff
Anil Kag <anilkagak2@gmail.com>
2021-06-11 22:37:25 -0400

    Results for bike dataset.

--- LWA_Regression/cqr/tune_lwa_run_one_experiment.py
+++ LWA_Regression/cqr/tune_lwa_run_one_experiment.py
@@ -7,7 +7,7 @@ import warnings
 warnings.filterwarnings("ignore")
 
 from joblib import Parallel, delayed
-
+import gc 
 import os
 import six
 import sys
@@ -64,12 +64,15 @@ import random
 n_jobs=10
 n_vals =20
 
-significance_list = [0.90, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99]
+#significance_list = [0.90, 0.91, 0.92, 0.93, 0.94, 0.95, 0.96, 0.97, 0.98, 0.99]
+significance_list = [0.96, 0.97, 0.98]
+#significance_list = [ 0.99]
 #significance_list = [0.99] #, 0.925, 0.95, 0.975]
 
 #dataset_name = 'concrete'
 dataset_name = 'bike' #'concrete'
 test_method = 'cqr_quantile_net'
+'''
 for significance in significance_list:
   target_cov = 100*significance
   max_cov, max_len, max_theta = None, None, None
@@ -81,6 +84,7 @@ for significance in significance_list:
   results = Parallel(n_jobs=n_jobs)(delayed( val_run_experiment )(  dataset_name, test_method, random.randint(1, 99), 
                       quantiles_net=[ theta, 1. - 2*alpha + theta ], significance=alpha, theta=theta, 
                       save_to_csv=False ) for theta in np.linspace(0.0, 2*alpha, num=n_vals))
+  gc.collect()
 
   for (cur_cov, cur_len, theta) in results:
     #cur_cov, cur_len = val_run_experiment(dataset_name, test_method, random_state, significance=alpha, quantiles_net=quantiles_net)
@@ -88,12 +92,15 @@ for significance in significance_list:
         max_cov, max_len, max_theta = cur_cov, cur_len, theta
     elif ( cur_cov >= target_cov ) and ( cur_len <= max_len ):
         max_cov, max_len, max_theta = cur_cov, cur_len, theta
+  del results
+  gc.collect()
   print(' --> CQR Net --> significance, --> max_cov, max_len, max_theta, 0.8+theta ',  significance, max_cov, max_len, max_theta, 1. - 2*alpha+max_theta )
 #assert(1==2)
 
   results = Parallel(n_jobs=n_jobs)(delayed( run_experiment )(  dataset_name, test_method, random_state, 
                   quantiles_net=[ max_theta, 1. - 2*alpha + max_theta ], significance=alpha, 
                   flush_summary=False, save_to_csv=False ) for random_state in random_state_train_test)
+  gc.collect()
   #print(results)
   #print(' len(results) ', len(results))
   #assert(1==2)
@@ -101,6 +108,8 @@ for significance in significance_list:
   for df2 in results[1:]:
       df = pd.concat([df2, df], ignore_index=True)
 
+  del results
+  gc.collect()
   print(df)
   #out_name = outdir + 'results-' + dataset_name  + '-significance-parallel.csv'
   out_name = outdir + 'results-' + dataset_name  + '-significance-tmp.csv'
@@ -110,7 +119,7 @@ for significance in significance_list:
                 df = pd.concat([df2, df], ignore_index=True)
 
   df.to_csv(out_name, index=False)
-
+'''
 
 
 #n_vals=40
@@ -128,6 +137,7 @@ for significance in significance_list:
                       _lambda2=( ( theta ) / ( 1. - theta ) ), theta=theta,
                       save_to_csv=False ) for theta in np.linspace(1.0 - 2*alpha, 1.0 - (alpha/2.5), num=n_vals)  )
 
+  gc.collect()
   for (cur_cov, cur_len, theta) in results:
   #for theta in np.linspace(1.0 - 2*alpha, 1.0 - (alpha/2.5), num=n_vals):
     #_lambda = theta
@@ -137,17 +147,22 @@ for significance in significance_list:
         max_cov, max_len, max_theta = cur_cov, cur_len, theta
     elif ( cur_cov >= target_cov ) and ( cur_len <= max_len ):
         max_cov, max_len, max_theta = cur_cov, cur_len, theta
+  del results
+  gc.collect()
   print(' --> LWA Net --> significance --> max_cov, max_len, max_lambda ',  significance, max_cov, max_len, max_theta )
 
   results = Parallel(n_jobs=n_jobs)(delayed( run_experiment )(  dataset_name, test_method, random_state, 
                   significance=alpha,  _lambda1=( ( max_theta ) / ( 1. - max_theta ) ),
                       _lambda2=( ( max_theta ) / ( 1. - max_theta ) ),
                   flush_summary=False, save_to_csv=False ) for random_state in random_state_train_test)
+  gc.collect()
   df = results[0]
   for df2 in results[1:]:
       df = pd.concat([df2, df], ignore_index=True)
       #print(results)
 
+  del results
+  gc.collect()
   #print(df)
   #out_name = outdir + 'results-' + dataset_name  + '-significance-parallel.csv'
   out_name = outdir + 'results-' + dataset_name  + '-significance-tmp.csv'
